3a0c3f4336949f3e63ae16bee3e194bb0f4e0e3f
Chinmay Anand <chinmayanand896@gmail.com>
2019-02-09 01:58:52 +0530

    re-commit

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Favorite;
+use App\Reply;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+
+    // for this instance creating a reply as favorite reply
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function store(Reply $reply)
+    {
+        $reply->favorite();
+
+        return back();
+    }
+}
